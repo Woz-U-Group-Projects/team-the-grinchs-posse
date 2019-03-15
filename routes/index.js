dbcc51223e3a98573da0f69f27a505d0aa7a174d
Jane Gotshalk <jane.gotshalk@my.scitexas.edu>
2019-03-15 16:12:12 -0400

    message

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var users = require('../models/member');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+ router.get('/artists', function(req, res, next) {
+      models.member.findAll({}).then(memberAsPlainObject => {
+        const mappedMember = memberAsPlainObject.map(member => ({
+          memberId: member.memberd,
+          Name: member.Name
+        }));
+        res.send(JSON.stringify(mappedArtists));
+      });
+    });
+  
+router.post('/member', (req, res) => {
+  models.member
+    .findOrCreate({
+      where: {
+        Name: req.body.name,
+        DateFormed: req.body.dateFormed
+      }
+    })
+    .spread(function(result, created) {
+      if (created) {
+        res.redirect('/member');
+      } else {
+        res.send('This member already exists!');
+      }
+    });
 });
 
 module.exports = router;
