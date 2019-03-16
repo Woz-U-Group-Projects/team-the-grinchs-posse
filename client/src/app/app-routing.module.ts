7e7c304537c492a2553af05170a52a2807d27152
Jesse Lind <jesse.lind@gmail.com>
2019-03-16 14:32:46 -0600

    added login and register components to app-routing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -4,9 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { MembersComponent } from './members/members.component';
 import { MemberDetailComponent } from './member-detail/member-detail.component';  
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
   { path: 'members', component: MembersComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'detail/:id', component: MemberDetailComponent }
