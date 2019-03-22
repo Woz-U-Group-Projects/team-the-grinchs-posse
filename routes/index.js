7c24fdde848e53a9a26356b74784f1eb9444852d
Jesse Lind <jesse.lind@gmail.com>
2019-03-22 07:14:58 -0600

    new config, models, auth.js, updated index/users

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,44 @@
+// ORIGINAL code:
+// var express = require('express');
+// var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+// module.exports = router;
+
+// Jane's code:
 var express = require('express');
 var router = express.Router();
+var users = require('../models/member');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/artists', function (req, res, next) {
+  models.member.findAll({}).then(memberAsPlainObject => {
+    const mappedMember = memberAsPlainObject.map(member => ({
+      memberId: member.memberd,
+      Name: member.Name
+    }));
+    res.send(JSON.stringify(mappedArtists));
+  });
+});
+
+router.post('/member', (req, res) => {
+  models.member
+    .findOrCreate({
+      where: {
+        Name: req.body.name,
+        DateFormed: req.body.dateFormed
+      }
+    })
+    .spread(function (result, created) {
+      if (created) {
+        res.redirect('/member');
+      } else {
+        res.send('This member already exists!');
+      }
+    });
 });
 
 module.exports = router;
