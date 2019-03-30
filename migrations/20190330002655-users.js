c4064f8b5a6bf9966a84c88bce2a1c5152355f97
Robert Hieger <rhieger@gmail.com>
2019-03-30 01:57:46 -0400

    Completed Sequelize migration  script for users table.

--- migrations/20190330002655-users.js
+++ migrations/20190330002655-users.js
@@ -2,22 +2,44 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-  },
+    return [
+      queryInterface.createTable(
+        'users',
+        {
+          userId:  {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+          },
+          firstName:   {
+            type: Sequelize.STRING,
+            allowNull: false
+          },
+          lastName:  {
+            type: Sequelize.STRING,
+            allowNull: false
+          },
+          userName:  {
+            type: Sequelize.STRING,
+            allowNull: false
+          },
+          password:  {
+            type: Sequelize.STRING,
+            allowNull: false
+          }
+        },
+        {
+          engine: 'InnoDB',   // default storage engine
+          charset: 'utf8'
+        }
+      )
+    ];
+  },  // end up:
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
+    return [
+      [queryInterface.dropTable('users')]
+    ];
   }
-};
+};  // end down:
