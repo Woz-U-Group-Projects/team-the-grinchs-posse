b390360bb7cc397f69b42859e74b688d751d825f
Robert Hieger <rhieger@gmail.com>
2019-03-30 02:43:44 -0400

    Completed sampleUsers seed for user table sample data.

--- seeders/20190330062426-sampleUsers.js
+++ seeders/20190330062426-sampleUsers.js
@@ -1,28 +1,50 @@
 // sampleUsers Seed for sample user table data
+//
+// Robert Hieger
+//
+// March 30, 2019
 
 'use strict';
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
+    return queryInterface.bulkInsert(
+      'users',
+      [
+        {
+          firstName: 'Robert',
+          lastName: 'Hieger',
+          userName: 'rlhieger',
+          password: '$@mpleP@$$wd'
+        },
+        {
+          firstName: 'Jane',
+          lastName: 'Gotshalk',
+          userName: 'jgotshalk',
+          password: '$@mpleP@$$wd'
+        },
+        {
+          firstName: 'Jesse',
+          lastName: 'Linde',
+          userName: 'jlinde',
+          password: '$@mpleP@$$wd'
+        },
+        {
+          firstName: 'Daniel',
+          lastName: 'Dash',
+          userName: 'dandash',
+          password: '$@mpleP@$$wd'
+        },
+        {
+          firstName: 'Patrick',
+          lastName: 'Jarolim, Jr.',
+          userName: 'pjarolim',
+          password: '$@mpleP@$$wd'
+        }
+      ], {});
+  },    // end up:
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
+    return  queryInterface.bulkDelete( 'users', null, {} );  
+  }   // end down:
 };
