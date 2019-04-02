fcdb7201c32041527ac7ec91db068508d6210e9a
Jesse Lind <jesse.lind@gmail.com>
2019-04-02 16:15:44 -0600

    redo register.component.ts

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app-routing.module';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,10 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Location } from '@angular/common';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { first } from 'rxjs/operators';
-import { Member } from '../member';
-import { MemberService } from '../member.service';
+
+import { User } from '../user';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-register',
@@ -12,26 +10,22 @@ import { MemberService } from '../member.service';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
-  registerMember: Member = new Member();
-
-  register() {
-    this.memberService.register(this.registerMember).subscribe();
-    // send registerMember to the service
-    // memberService.register(registerMember).subscribe();
-  }
+  // initialize an empty user object
+  // we need to do this so the property binding works [(ngModel)]
+  user: User = new User();
 
   constructor(
-    private router: Router,
-    private location: Location,
-    private memberService : MemberService
-  ) { }
+    private userService: UserService,
+    private router: Router
+    ) {}
+
+    register(): void {
+      this.userService.registerUser(this.user).subscribe(() => {
+        // user registered, send them to the login page
+        this.router.navigate(['/login']);
+      })
+    }
 
   ngOnInit() {
   }
-
-  // goBack(): void {
-  //   this.location.back();
-  // }
-
 }
