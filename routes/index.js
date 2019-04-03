15c16df2b5d5e4fa1bd71adad834893bfb09e73b
Jesse Lind <jesse.lind@gmail.com>
2019-04-03 15:05:51 -0600

    update index.js and users.js in routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,44 +1,9 @@
-// ORIGINAL code:
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.send("hello!");
 });
 
 module.exports = router;
-
-// /////// Jane's code: ///////
-// var express = require('express');
-// var router = express.Router();
-// var users = require('../models');
-
-// router.get('/members', function (req, res, next) {
-//   models.member.findAll({}).then(memberAsPlainObject => {
-//     const mappedMember = memberAsPlainObject.map(member => ({
-//       memberId: member.memberId,
-//       Name: member.Name
-//     }));
-//     res.send(JSON.stringify(mappedMembers));
-//   });
-// });
-
-// router.post('/member', (req, res) => {
-//   models.member
-//     .findOrCreate({
-//       where: {
-//         Name: req.body.name,
-//         DateFormed: req.body.dateFormed
-//       }
-//     })
-//     .spread(function (result, created) {
-//       if (created) {
-//         res.redirect('/member');
-//       } else {
-//         res.send('This member already exists!');
-//       }
-//     });
-// });
-
-// module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,240 +1,119 @@
-// ORIGINAL code:
-var express = require('express');
+var express = require("express");
 var router = express.Router();
+var models = require("../models"); //<--- Add models
+var authService = require("../services/auth"); //<--- Add authentication service
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// create new user if one doesn't exist
+// attempt to find the user by their username, then add the rest of the values from the request
+router.post("/signup", function(req, res, next) {
+  models.users
+    .findOrCreate({
+      where: {
+        username: req.body.username
+      },
+      defaults: {
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: authService.hashPassword(req.body.password) // encrypt the user password
+      }
+    })
+    .spread(function(result, created) {
+      if (created) {
+        // if the user was created, positive response
+        res.json("User successfully created");
+      } else {
+        // if the user was not created, negative response
+        res.json("This user already exists");
+      }
+    });
+});
+
+// login user and return JWT as cookie
+// attempt to find the user by their username, if not found then respond "User not found"
+//
+router.post("/login", function(req, res, next) {
+  models.users
+    .findOne({
+      where: {
+        username: req.body.username
+      }
+    })
+    .then(user => {
+      if (!user) {
+        console.log("User not found");
+        return res.status(401).json("Login Failed");
+      } else {
+        // check to see if the passwords match
+        let passwordMatch = authService.comparePasswords(
+          req.body.password,
+          user.password
+        );
+        if (passwordMatch) {
+          // passwords match, create a jwt token as a cookie and attach to the response
+          let token = authService.signUser(user);
+          res.cookie("jwt", token);
+          res.json("Login successful");
+        } else {
+          // wrong password, negative response
+          console.log("Wrong password");
+          res.json("Wrong password");
+        }
+      }
+    });
+});
+
+// find a profile from a user (their user object) based on the received jtw cookie
+router.get("/profile", function(req, res, next) {
+  // read the cookie from the request
+  let token = req.cookies.jwt;
+  // if we have a cookie we can proceed
+  if (token) {
+    // validate the cookie
+    authService.verifyUser(token).then(user => {
+      if (user) {
+        // empty the password field, do not send this property to the front-end
+        user.password = "";
+        // return the user object
+        res.send(JSON.stringify(user));
+      } else {
+        res.status(401);
+        res.json("Invalid authentication token");
+      }
+    });
+  } else {
+    // no jwt cookie, assume user is not logged in
+    res.status(401);
+    res.json("Must be logged in");
+  }
+});
+
+// logout
+router.get("/logout", function(req, res, next) {
+  // set a new jwt cookie that will immediately expire
+  res.cookie("jwt", "", { expires: new Date(0) });
+  res.json("Logged out");
+});
+
+// validate a token
+router.get("/validateToken", function(req, res, next) {
+  // check to see if there is a token
+  let token = req.cookies.jwt;
+  if (token) {
+    // validate the user from the token (same as finding profile)
+    authService.verifyUser(token).then(user => {
+      if (user) {
+        // token valid, return true
+        res.json(true);
+      } else {
+        // token invalid, return false
+        res.json(false);
+      }
+    });
+  } else {
+    // no token, return false
+    res.json(false);
+  }
 });
 
 module.exports = router;
-
-// /////// Jane's code: ///////
-// var express = require('express');
-// var router = express.Router();
-// var models = require('../models');
-// const auth = require('../config/auth');
-
-
-// /* GET users listing. */
-// router.get('/', function (req, res, next) {
-//   res.send('respond with a resource');
-// });
-// router.get('/signup', function (req, res, next) {
-//   res.render('signup')
-// });
-
-// router.post('/signup', function (req, res, next) {
-//   models.users.findOne({
-//     where: {
-//       Username: req.body.username
-//     }
-//   }).then(user => {
-//     if (user) {
-//       res.send('this user already exists')
-//     } else {
-//       models.users.create({
-//         FirstName: req.body.firstName,
-//         LastName: req.body.lastName,
-//         Email: req.body.email,
-//         Username: req.body.username,
-//         Password: req.body.password
-//       }).then(createdUser => {
-
-//         //Add code here
-
-//       });
-//     }
-//   });
-
-// });
-
-// router.get('/login', function (req, res, next) {
-//   res.render('login');
-// });
-
-// // This code is redundant:
-// // router.post('/login', function (req, res, next) {
-// //   models.users.findOne({
-// //     where: {
-// //       Username: req.body.username,
-// //       Password: req.body.password
-// //     }
-// //   }).then(user => {
-// //     console.log('login found a user')
-// //     if (!user) {
-// //       return res.status(401).json({
-// //         message: "Login Failed"
-// //       });
-// //     }
-// //     if (user) {
-// //       //Add code here
-
-// //     } else {
-// //       console.log(req.body.password);
-// //       res.redirect('login')
-// //     }
-
-// //   });
-// // });
-
-// router.get('/profile/:id', auth.verifyUser, function (req, res, next) {
-//   if (req.params.id !== String(req.user.UserId)) {
-//     res.send('This is not your profile')
-//   } else {
-//     res.render('profile', {
-//       FirstName: req.user.FirstName,
-//       LastName: req.user.LastName,
-//       Email: req.user.Email,
-//       UserId: req.user.UserId,
-//       Username: req.user.Username
-//     });
-//   }
-
-// });
-
-// module.exports = router;
-
-
-// router.post('/signup', function (req, res, next) {
-//   models.users.findOne({
-//     where: {
-//       Username: req.body.username
-//     }
-//   }).then(user => {
-//     if (user) {
-//       res.send('this user already exists')
-//     } else {
-//       models.users.create({
-//         FirstName: req.body.firstName,
-//         LastName: req.body.lastName,
-//         Email: req.body.email,
-//         Username: req.body.username,
-//         Password: req.body.password
-//       }).then(createdUser => {
-//         if (createdUser) {
-//           const userId = createdUser.UserId
-//           const token = auth.signUser(createdUser);
-//           res.cookie('jwt', token);
-//           res.redirect('profile/' + userId)
-//         } else {
-//           console.error('not a match');
-//         }
-
-//       });
-//     }
-//   });
-
-// });
-
-// router.post('/signup', function (req, res, next) {
-//   const hashedPassword = auth.hashPassword(req.body.password);
-//   models.users
-//     .findOne({
-//       where: {
-//         Username: req.body.username
-//       }
-//     })
-//     .then(user => {
-//       if (user) {
-//         res.send('this user already exists');
-//       } else {
-//         models.users
-//           .create({
-//             FirstName: req.body.firstName,
-//             LastName: req.body.lastName,
-//             Email: req.body.email,
-//             Username: req.body.username,
-//             Password: hashedPassword
-//           })
-//           .then(createdUser => {
-//             const isMatch = createdUser.comparePassword(req.body.password);
-
-//             if (isMatch) {
-//               const userId = createdUser.UserId;
-//               console.log(userId);
-//               const token = auth.signUser(createdUser);
-//               res.cookie('jwt', token);
-//               res.redirect('profile/' + userId);
-//             } else {
-//               console.error('not a match');
-//             }
-//           });
-//       }
-//     });
-// });
-
-// router.post('/login', function (req, res, next) {
-//   models.users.findOne({
-//     where: {
-//       Username: req.body.username,
-//       Password: req.body.password
-//     }
-//   }).then(user => {
-//     console.log('login found a user')
-//     if (!user) {
-//       return res.status(401).json({
-//         message: "Login Failed"
-//       });
-//     }
-//     if (user) {
-//       const userId = user.UserId;
-//       const token = auth.signUser(user);
-//       res.cookie('jwt', token);
-//       res.redirect('profile/' + userId)
-//     } else {
-//       console.log(req.body.password);
-//       res.redirect('login')
-//     }
-
-//   });
-// });
-
-// router.post('/login', function (req, res, next) {
-//   const hashedPassword = auth.hashPassword(req.body.password);
-//   models.users.findOne({
-//     where: {
-//       Username: req.body.username
-//     }
-//   }).then(user => {
-//     const isMatch = user.comparePassword(req.body.password)
-
-//     if (!user) {
-//       return res.status(401).json({
-//         message: "Login Failed"
-//       });
-//     }
-//     if (isMatch) {
-//       const userId = user.UserId
-//       const token = auth.signUser(user);
-//       res.cookie('jwt', token);
-//       res.redirect('profile/' + userId)
-//     } else {
-//       console.log(req.body.password);
-//       res.redirect('login')
-//     }
-
-//   });
-// });
-
-// router.get('/logout', function (req, res) {
-//   res.cookie('jwt', null);
-//   res.redirect('/users/login');
-// });
-
-// users.associate = function (models) {
-//   // associations can be defined here
-// };
-
-// users.associate = function (models) {
-//   // associations can be defined here
-// };
-
-// users.prototype.comparePassword = function (plainTextPassword) {
-//   let user = this;
-//   return bcrypt.compareSync(plainTextPassword, user.Password)
-// };
-
-// return users;
-
-// module.exports = router;
