4b4a16487d1466183dbdc661c2fc2d9c052b5106
Robert Hieger <rhieger@gmail.com>
2019-04-03 00:31:57 -0400

    Completed model and migration for users table.

--- migrations/20190403042248-create-users.js
+++ migrations/20190403042248-create-users.js
@@ -2,32 +2,31 @@
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', {
-      id: {
+      userId: {
         allowNull: false,
         autoIncrement: true,
         primaryKey: true,
         type: Sequelize.INTEGER
       },
       firstName: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       lastName: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       userName: {
+        allowNull: false,
         type: Sequelize.STRING
       },
       password: {
+        allowNull: false,
         type: Sequelize.STRING
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
       }
+    },
+    {
+        timestamps: false
     });
   },
   down: (queryInterface, Sequelize) => {
