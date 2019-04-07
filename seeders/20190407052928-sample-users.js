1a6f7b468addbb9314d2035b4a889045ee762912
Robert Hieger <rhieger@gmail.com>
2019-04-07 01:35:30 -0400

    Refactored seeder for users to populate with sample data.

--- seeders/20190407052928-sample-users.js
+++ seeders/20190407052928-sample-users.js
@@ -2,25 +2,46 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+    
+    return queryInterface.bulkInsert('users', [
+      {
+        firstName: 'Robert',
+        lastName: 'Hieger',
+        userName: 'rlhieger',
+        password: '$@mpleP@sswd'
+      },
+      {
+        firstName: 'Jane',
+        lastName: 'Gotshalk',
+        userName: 'jgotshalk',
+        password: '$@mpleP@sswd'
+      },
+      {
+        firstName: 'Jesse',
+        lastName: 'Linde',
+        userName: 'jlinde',
+        password: '$@mpleP@sswd'
+      },
+      {
+        firstName: 'Daniel',
+        lastName: 'Dash',
+        userName: 'dandash',
+        password: '$@mpleP@sswd'
+      },
+      {
+        firstName: 'Patrick',
+        lastName: 'Jarolim, Jr.',
+        userName: 'pjarolim',
+        password: '$@mpleP@sswd'
+      }
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+  ], {});
+    
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    
+    return queryInterface.bulkDelete( 'users', null, {} );
+    
   }
 };
