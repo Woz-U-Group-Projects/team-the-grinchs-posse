20e432f6ffac3900881a8aa938199c52a25f9b31
Robert Hieger <rhieger@gmail.com>
2019-04-07 01:20:43 -0400

    Generated stub model for users table.

---
+++ models/users.js
@@ -0,0 +1,14 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const users = sequelize.define('users', {
+    userId: DataTypes.INTEGER,
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    userName: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {});
+  users.associate = function(models) {
+    // associations can be defined here
+  };
+  return users;
+};
