9216d029886d941c5e50beb3c57ea037e99f9733
Robert Hieger <rhieger@gmail.com>
2019-04-07 02:02:03 -0400

    Refactored seeder for giftLists table to populate with sample data.

--- seeders/20190407055624-sample-giftLists.js
+++ seeders/20190407055624-sample-giftLists.js
@@ -2,25 +2,40 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
+    return queryInterface.bulkInsert('giftLists', [
+      
+      {
+        listTitle: 'Bobby\'s Wish List',
+        listOwner: 1
+      },
+      {
+        listTitle: 'Jane\'s Wish List',
+        listOwner: 2
+      },
+      {
+        listTitle: 'Jesse\'s Wish List',
+        listOwner: 3
+      },
+      {
+        listTitle: 'Dan\'s Wish List',
+        listOwner: 4
+      },
+      {
+        listTitle: 'Patrick\'s Wish List',
+        listOwner: 5
+      }
+
+    ],
+    
+    {});
+
   },
 
   down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
+    return queryInterface.bulkDelete( 'giftLists', null, {} );
+  
   }
+
 };
