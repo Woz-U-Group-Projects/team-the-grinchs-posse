b3b5a0d6a620c9f5396f04db9ed7204420f7c706
Robert Hieger <rhieger@gmail.com>
2019-04-07 01:25:01 -0400

    Configured model for users table.

--- models/users.js
+++ models/users.js
@@ -1,12 +1,31 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const users = sequelize.define('users', {
-    userId: DataTypes.INTEGER,
-    firstName: DataTypes.STRING,
-    lastName: DataTypes.STRING,
-    userName: DataTypes.STRING,
-    password: DataTypes.STRING
+
+    userId: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true
+    },
+    firstName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    lastName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    userName: {
+      allowNull: false,
+      type: DataTypes.STRING
+    },
+    password: {
+      allowNull: false,
+      type: DataTypes.STRING
+    }
+
   }, {});
+  
   users.associate = function(models) {
     // associations can be defined here
   };
